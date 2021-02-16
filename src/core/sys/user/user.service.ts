b86705f30abcab2e1a94ab74078bbaa78d70f14c
eightdoor <851708184@qq.com>
2021-02-16 13:15:55 +0800

    fix

--- src/core/sys/user/user.service.ts
+++ src/core/sys/user/user.service.ts
@@ -5,10 +5,11 @@ import { TypeOrmCrudService } from '@nestjsx/crud-typeorm';
 import { Repository } from 'typeorm';
 import { SysUser } from './user.entity';
 import utils from 'src/utils/index';
+import { SysUserRole } from './userRole.entity';
 
 @Injectable()
 export class UserService extends TypeOrmCrudService<SysUser> {
-  constructor(@InjectRepository(SysUser) repo: Repository<SysUser>) {
+  constructor(@InjectRepository(SysUser) repo: Repository<SysUser>, @InjectRepository(SysUserRole) private readonly userRole: Repository<SysUserRole>) {
     super(repo)
   }
   async createOne(req: CrudRequest, dto: SysUser): Promise<SysUser> {
@@ -24,5 +25,12 @@ export class UserService extends TypeOrmCrudService<SysUser> {
     const { id } = this.getParamFilters(req.parsed)
     await this.repo.update(id, dto)
     return dto
+  }
+  async deleteOne(req: CrudRequest): Promise<SysUser> {
+    const params = this.getParamFilters(req.parsed)
+    // 先删除用户存在的角色
+    await this.userRole.createQueryBuilder().where("user_id = :id", { id: params.id }).delete().execute()
+    const reuslt = await this.repo.delete(params.id)
+    return reuslt.raw
   }
 }
